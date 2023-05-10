b5572282512db9b973ec6801a3bd9c0ac1738e7d
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-05-10 15:18:56 +0200

    toggleActions

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ gsap.to('.b2 h2', {
     y: 0,
     scale: 2,
     autoAlpha: 1,
-    scrollTrigger: ".b2 h2",
+    scrollTrigger: {
+        trigger: '.b2 h2',
+        markers: true,
+        // trigger scroller
+        start: 'top 60%',
+        end: 'bottom 40%',
+        // onEnter onLeave onEnterBack onLeaveBack
+        toggleActions: 'play none reverse reverse',
+        onEnter: () => console.log('enter'),
+        onLeave: () => console.log('leave'),
+        onEnterBack: () => console.log('enterBack'),
+        onLeaveBack: () => console.log('leaveBack'),
+    },
 })
