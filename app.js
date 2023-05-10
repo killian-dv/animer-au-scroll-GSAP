afb37295b85d6e8cff21801f946c4c73a80ed34a
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-05-10 16:10:44 +0200

    découverte pin et scrub

--- app.js
+++ app.js
@@ -5,15 +5,30 @@ gsap.to('.b2 h2', {
     autoAlpha: 1,
     scrollTrigger: {
         trigger: '.b2 h2',
-        markers: true,
+        // markers: true,
         // trigger scroller
-        start: 'top 60%',
-        end: 'bottom 40%',
+        start: 'top 20%',
+        end: 'bottom 10%',
         // onEnter onLeave onEnterBack onLeaveBack
         toggleActions: 'play none reverse reverse',
         onEnter: () => console.log('enter'),
         onLeave: () => console.log('leave'),
         onEnterBack: () => console.log('enterBack'),
         onLeaveBack: () => console.log('leaveBack'),
+        scrub: 1,
     },
+})
+
+// gsap.to('.b2', {
+//     scrollTrigger: {
+//         trigger: '.b2',
+//         pin: true,
+//         markers: true,
+//     },
+// })
+
+ScrollTrigger.create({
+    trigger: '.b2',
+        pin: true,
+        markers: true,
 })
